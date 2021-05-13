ff8fa8d47adbc128beec07b626057c95376dddca
Stefano Balieti <futur.dorko@gmail.com>
2021-05-13 19:40:06 +0200

    ionic

---
+++ 13_Ionic/app.js
@@ -0,0 +1,18 @@
+const reasonInput = document.querySelector('#input-reason');
+const amountInput = document.querySelector('#input-amount');
+const cancelBtn = document.querySelector('#btn-cancel');
+const confirmBtn = document.querySelector('#btn-confirm');
+
+confirmBtn.addEventListener('click', () => {
+  const enteredReason = reasonInput.value;
+  const enteredAmount = amountInput.value;
+
+  if (
+    enteredReason.trim().length <= 0 ||
+    enteredAmount <= 0 ||
+    enteredAmount.trim().length <= 0
+  ) {
+    return;
+  }
+  console.log(enteredReason, enteredAmount);
+});
