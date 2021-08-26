447082ada1074199f759ba925be22113bcaf932e
Stefano Balietti <futur.dorko@gmail.com>
2021-08-26 11:08:35 +0200

    __dirname reference

--- 3_NPM/2_require.js
+++ 3_NPM/2_require.js
@@ -27,30 +27,28 @@
 
 
 // // Ref: https://nodejs.org/api/path.html
+// // Ref: https://www.digitalocean.com/community/tutorials/nodejs-how-to-use__dirname
 
 // // EXERCISE 2. Export your own file.
 // ////////////////////////////////////
 
 // // Sometimes one export needs to return multiple variables, and not just
-// // one function as in the "misteryFile.js" of Exercise 1. 
-// // Create a new file inside the lib/ folder and name it "persons.js". This 
+// // one function as in the "misteryFile.js" of Exercise 1.
+// // Create a new file inside the lib/ folder and name it "persons.js". This
 // // file must export two objects containing persons metadata of the type
-// // 
+// //
 // // { first: 'Brendan', last: 'Eich' }
 // //
 // // { first: 'Bill', last: 'Gates' }
-// // 
-// // and one method to get a random person object. In other words, the code below 
+// //
+// // and one method to get a random person object. In other words, the code below
 // // must run.
 
 // // Hint: the code below use object destructuring, ref:
 // // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
- 
+
 let { person1, person2, getRandomPerson } = require('./lib/persons.js');
 
 console.log(person1);
 console.log(person2);
 console.log(getRandomPerson());
-
-
-
